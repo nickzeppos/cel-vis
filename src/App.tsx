3ad12a84b84b9a26a834cd881a6a1c5776068b2d
Nick Zeppos <zepposn@gmail.com>
2025-04-21 11:50:58 -0400

    send height to parent to allow iframe to dynamically adjust its height based on app height. to get rid of double scroll bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import type { ViewRoute } from "@/lib/types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FederalTableView from "@/components/federal/FederalTableView";
 import { FederalScorecardView } from "@/components/federal/FederalScorecardView";
 import StateTableView from "./components/state/StateTableView";
@@ -8,6 +8,35 @@ import { StateScorecardView } from "./components/state/StateScorecardView";
 const defaultRoute = { type: "federal:table" } as ViewRoute;
 function App() {
   const [route, setRoute] = useState<ViewRoute>(defaultRoute);
+
+  useEffect(() => {
+    // Function to send height to parent
+    const sendHeightToParent = () => {
+      const height = document.body.scrollHeight;
+      if (window.parent) {
+        window.parent.postMessage(
+          {
+            type: "setHeight",
+            height: height,
+          },
+          "*" // TODO: control target origin w/ env
+        );
+      }
+    };
+
+    // Send height when component mounts
+    sendHeightToParent();
+
+    // Send height when window resizes
+    window.addEventListener("resize", sendHeightToParent);
+
+    // Optional: Send height when content changes
+    // You might need to call this function after data loads or UI updates
+
+    return () => {
+      window.removeEventListener("resize", sendHeightToParent);
+    };
+  }, []);
 
   // switch render on route
   switch (route.type) {
