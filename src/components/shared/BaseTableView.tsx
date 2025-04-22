f4f74bb803ea4165c3a2709857ebe136b4b64f39
Nick Zeppos <zepposn@gmail.com>
2025-04-22 14:44:25 -0400

    Organize imports

--- src/components/shared/BaseTableView.tsx
+++ src/components/shared/BaseTableView.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import { cn } from "@/lib/utils";
-import { container, gridTemplates, sizes } from "@/lib/layout";
+import { container } from "@/lib/layout";
 
 interface BaseTableViewProps {
   title: string;
